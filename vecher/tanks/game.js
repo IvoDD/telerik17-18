16b17dfb04d3f6bfc015d38e31a1106b8bca9918
Ivo Dilov <iddilov@gmail.com>
2018-02-25 19:01:09 +0200

    tanks

--- vecher/tanks/game.js
+++ vecher/tanks/game.js
@@ -19,9 +19,28 @@
         context.fill();
     }
 };
+
+class Bullet{
+    constructor(x_, y_, tx, ty){
+        this.x = x_;
+        this.y = y_;
+        let dist = Math.sqrt((tx-x_)*(tx-x_) + (ty-y_)*(ty-y_));
+        this.dx = (tx-x_)/dist*10;
+        this.dy = (ty-y_)/dist*10;
+    }
+    update(){
+        this.x += this.dx;
+        this.y += this.dy;
+    }
+    draw(){
+        context.fillStyle = "black";
+        context.fillRect(this.x, this.y, 10, 10);
+    }
+};
 var colors = ['blue', 'red', 'green', 'yellow', 'pink'];
 var tanks = [];
 var rot = [];
+var bullet = [];
 tanks[0] = new Tank(400, 300, 'red');
 for (let i=1; i<10; ++i){
     tanks[i] = new Tank(Math.random()*800, Math.random()*600, 'blue');
@@ -43,12 +62,21 @@ function update() {
         dx *= rot[i]; dy *= rot[i];
         tanks[i].move(dx, dy);
         if (Math.random()<0.01){rot[i]*=-1;}
+        if (Math.random()<0.1){
+            bullet.push(new Bullet(tanks[i].x, tanks[i].y, tanks[0].x, tanks[0].y));
+        }
+    }
+    for (let i=0; i<bullet.length; ++i){
+        bullet[i].update();
     }
 }
 
 function draw() {
     for (let i=0; i<10; ++i){
         tanks[i].draw();
+    }
+    for (let i=0; i<bullet.length; ++i){
+        bullet[i].draw();
     }
 };
 
@@ -58,6 +86,7 @@ function keyup(key) {
 };
 
 function mouseup() {
+    bullet.push(new Bullet(tanks[0].x, tanks[0].y, mouseX, mouseY));
     // Show coordinates of mouse on click
     console.log("Mouse clicked at", mouseX, mouseY);
 };
