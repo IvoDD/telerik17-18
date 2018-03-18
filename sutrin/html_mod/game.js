55f58a976bee67af1cdadcd10e58540eee1bb2e3
Ivo Dilov <iddilov@gmail.com>
2018-03-18 10:08:38 +0200

    html basics p2

---
+++ sutrin/html_mod/game.js
@@ -0,0 +1,40 @@
+﻿// Creating variables
+var myX = 0, myY = 0;
+var username = "";
+
+function startgame(){
+    username = document.getElementById('username').value;
+    if (username == "") return;
+    document.getElementById('form-id').style.display = 'none';
+    document.getElementById('button-id').style.display = 'none';
+    document.getElementById('canvas-id').style.display = 'block';
+    if (!localStorage[username]){
+        localStorage[username] = 0;
+    }
+}
+
+function update() {
+    myX = myX+(mouseX-myX)/10;
+    myY = myY+(mouseY-myY)/10;
+}
+
+function draw() {
+    // This is how you draw a rectangle
+    context.font = "30px Arial";
+    context.fillText(localStorage[username], 400, 100);
+    context.fillText(username, myX, myY-20);
+    context.fillRect(myX, myY, 30, 30);
+};
+
+function keyup(key) {
+    // Show the pressed keycode in the console
+    console.log("Pressed", key);
+};
+
+function mouseup() {
+    if (username != ""){
+        ++localStorage[username];
+    }
+    // Show coordinates of mouse on click
+    console.log("Mouse clicked at", mouseX, mouseY);
+};
