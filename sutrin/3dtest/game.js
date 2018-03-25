12e434b9c0c4d88e2f83f3f00bce1d283bec5bfe
Ivo Dilov <iddilov@gmail.com>
2018-03-25 11:06:56 +0300

    three.js examples

---
+++ sutrin/3dtest/game.js
@@ -0,0 +1,32 @@
+// Creating variables
+
+var geometry = new THREE.BoxGeometry( 0.9, 0.9, 0.9 );
+var material = new THREE.MeshPhongMaterial();
+var cube = new THREE.Mesh( geometry, material );
+cube.position.set(0, 0, 0);
+scene.add( cube );
+
+camera.position.set(7, 10, 16);
+camera.lookAt(new THREE.Vector3(0,0,0));
+
+var light = new THREE.PointLight( );
+var light2 = new THREE.PointLight( );
+light.position.set(8,8,12);
+light2.position.set(-8, -8, 12);
+scene.add( light );
+scene.add( light2 );
+
+function update() {
+	//cube.rotation.x += 0.015;
+	//cube.rotation.y += 0.010;
+	//cube.rotation.z += 0.005;
+}
+
+function keyup(key) {
+	// Show the pressed keycode in the console
+	console.log("Pressed", key);
+}
+function mouseup() {
+	// Show coordinates of mouse on click
+	console.log("Mouse clicked at", mouseX, mouseY);
+}
---
+++ sutrin/saves/game.js
@@ -0,0 +1,41 @@
+﻿// Creating variables
+var savename = "";
+var myX = 0, myY = 0;
+
+var saveList;
+//localStorage[savename] = {}
+if (!localStorage.saveList) saveList = [];
+else saveList = JSON.parse(localStorage.saveList);
+
+function goToNewSave(){
+    document.getElementById('saves-menu').style.display = 'none';
+    document.getElementById('savename-menu').style.display = 'block';
+}
+
+function startGame(){
+    savename = document.getElementById('save-name').value;
+    document.getElementById('savename-menu').style.display = 'none';
+    document.getElementById('canvas-id').style.display = 'block';
+    saveList.push(savename);
+    localStorage.saveList = JSON.stringify(saveList);
+}
+
+function update() {
+    myX = myX+(mouseX-myX)/10;
+    myY = myY+(mouseY-myY)/10;
+}
+
+function draw() {
+    // This is how you draw a rectangle
+    context.fillRect(myX, myY, 30, 30);
+};
+
+function keyup(key) {
+    // Show the pressed keycode in the console
+    console.log("Pressed", key);
+};
+
+function mouseup() {
+    // Show coordinates of mouse on click
+    console.log("Mouse clicked at", mouseX, mouseY);
+};
