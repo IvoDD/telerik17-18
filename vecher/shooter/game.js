c2fcdcc1a2fa29b418745b4f6e253f0360b253f3
Ivo Dilov <iddilov@gmail.com>
2018-04-01 18:36:42 +0300

    functional shooter

--- vecher/shooter/game.js
+++ vecher/shooter/game.js
@@ -1,14 +1,9 @@
 // Creating variables
 var socket = io();
 
+var bX = [], bY = [], bId = [];
 var x = [], y = [];
 var id = -1;
-
-/*function f(a, g){
-    return a+g(5);
-}
-
-console.log( f(3, function(x){return x*x}) );*/
 
 socket.on('id', function(cid, x_, y_){
     id = cid;
@@ -19,10 +14,23 @@ socket.on('id', function(cid, x_, y_){
 socket.on('coord', function(id_, x_, y_){
     x[id_] = x_;
     y[id_] = y_;
+});
+
+socket.on('bul', function(bX_, bY_, bId_){
+    bX = bX_;
+    bY = bY_;
+    bId = bId_;
 })
 
 function update() {
-    
+    let haveMoved = 0;
+    if (isKeyPressed[87]){y[id]-=5; haveMoved=1;}
+    if (isKeyPressed[83]){y[id]+=5; haveMoved=1;}
+    if (isKeyPressed[65]){x[id]-=5; haveMoved=1;}
+    if (isKeyPressed[68]){x[id]+=5; haveMoved=1;}
+    if (haveMoved){
+        socket.emit('move', id, x[id], y[id]);
+    }
 }
 
 function draw() {
@@ -31,11 +39,16 @@ function draw() {
         else{context.fillStyle = 'blue';}
         context.fillRect(x[i], y[i], 30, 30);
     }
+    for (let i=0; i<bX.length; ++i){
+        if (bId[i] == id) context.fillStyle = 'red';
+        else context.fillStyle = 'blue';
+        context.fillRect(bX[i], bY[i], 10, 10);
+    }
 }
 
 function keyup(key) {
 	
 }
 function mouseup() {
-    
+    socket.emit('shoot', mouseX, mouseY);
 }
--- vecher/shooter/server.js
+++ vecher/shooter/server.js
@@ -9,15 +9,80 @@ app.get('/game.js', function(req, res){
     res.sendFile(__dirname + "/game.js");
 });
 
+var bX = [], bY = [], bId = []; 
+var dx = [], dy = [];
 var x = [], y = []
 var num_clients = 0;
+var vel = 10;
 io.on('connection', function(socket){
     let cid = num_clients++, x_ = Math.random()*800, y_ = Math.random()*600;
     x[cid] = x_;
     y[cid] = y_;
     socket.emit('id', cid, x, y);
     io.emit('coord', cid, x_, y_);
+    socket.on('move', function(id, x1, y1){
+        x[id] = x1;
+        y[id] = y1;
+        io.emit('coord', id, x1, y1);
+    });
+    socket.on('disconnect', function(){
+        x[cid] = -100;
+        y[cid] = -100;
+        io.emit('coord', cid, -100, -100);
+    });
+    socket.on('shoot', function(tX, tY){
+        bX.push(x[cid]);
+        bY.push(y[cid]);
+        bId.push(cid);
+        let dist = Math.sqrt((tX - x[cid])*(tX - x[cid]) + (tY - y[cid])*(tY - y[cid]));
+        dx.push((tX-x[cid])*vel/dist);
+        dy.push((tY-y[cid])*vel/dist);
+    })
 });
+
+function areColliding(Ax, Ay, Awidth, Aheight, Bx, By, Bwidth, Bheight) {
+	if (Bx <= Ax + Awidth) {
+		if (Ax <= Bx + Bwidth) {
+			if (By <= Ay + Aheight) {
+				if (Ay <= By + Bheight) {
+					return 1;
+				}
+			}
+		}
+	}
+	return 0;
+}
+
+function upd(){
+    for (let i=0; i<bX.length; ++i){
+        bX[i] += dx[i];
+        bY[i] += dy[i];
+        if (bX[i] < -10 || bX[i] > 790 || bY[i] < -10 || bY[i] > 590){
+            bX[i] = bX[bX.length-1]; bX.pop();
+            bY[i] = bY[bY.length-1]; bY.pop();
+            bId[i] = bId[bId.length-1]; bId.pop();
+            dx[i] = dx[dx.length-1]; dx.pop();
+            dy[i] = dy[dy.length-1]; dy.pop();
+            --i;
+        }
+        for (let j=0; j<x.length; ++j){
+            if (j!=bId[i] && areColliding(bX[i], bY[i], 10, 10, x[j], y[j], 30, 30)){
+                x[j] = -100000000;
+                y[j] = -100000000;
+                io.emit('coord', j, x[j], y[j]);
+                bX[i] = bX[bX.length-1]; bX.pop();
+                bY[i] = bY[bY.length-1]; bY.pop();
+                bId[i] = bId[bId.length-1]; bId.pop();
+                dx[i] = dx[dx.length-1]; dx.pop();
+                dy[i] = dy[dy.length-1]; dy.pop();
+                --i;
+                break;
+            }
+        }
+    }
+    io.emit('bul', bX, bY, bId);
+}
+setInterval(upd, 10);
 
 http.listen(3000, function(){
     console.log("server started");
