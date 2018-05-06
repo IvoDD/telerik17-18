0521fe3d1cb8be15fb9de46e49be60f29c5346d4
Ivo Dilov <iddilov@gmail.com>
2018-05-06 19:04:24 +0300

    maze generate

--- vecher/maze3d/game.js
+++ vecher/maze3d/game.js
@@ -3,6 +3,65 @@ var canvas = document.getElementsByTagName("canvas")[0];
 var geometry = [new THREE.BoxGeometry( 10, 6, 1 ), new THREE.BoxGeometry( 1, 6, 10 )];
 var material = new THREE.MeshPhongMaterial();
 
+var n=20;
+var haswall0 = [], haswall1 = [];
+var used=[];
+var cand = [];
+
+for (let i=0; i<n; ++i){
+    used[i]=[];
+    for (let j=0; j<n; ++j){
+        used[i][j]=false;
+    }
+}
+
+for (let i=0; i<n; ++i){
+    haswall0[i] = []
+    for (let j=0; j<=n; ++j) haswall0[i][j] = true;
+}
+for (let i=0; i<=n; ++i){
+    haswall1[i] = []
+    for (let j=0; j<n; ++j) haswall1[i][j] = true;
+}
+
+function addUsed(i, j){
+    used[i][j] = true;
+    cand.push({t: 0, i: i, j: j});
+    cand.push({t: 0, i: i, j: j+1});
+    cand.push({t: 1, i: i, j: j});
+    cand.push({t: 1, i: i+1, j: j});
+}
+addUsed(n/2, n/2);
+while (cand.length > 0){
+    let ind = Math.floor(Math.random()*cand.length);
+    let c = cand[ind];
+    cand[ind] = cand[cand.length-1];
+    cand.pop();
+    if (c.t == 0 && c.j > 0 && c.j<n){
+        if (!used[c.i][c.j]){addUsed(c.i, c.j); haswall0[c.i][c.j]=false;}
+        if (!used[c.i][c.j-1]){addUsed(c.i, c.j-1); haswall0[c.i][c.j]=false;}
+    }
+    if (c.t == 1 && c.i > 0 && c.i<n){
+        if (!used[c.i][c.j]) {addUsed(c.i, c.j); haswall1[c.i][c.j]=false;}
+        if (!used[c.i-1][c.j]) {addUsed(c.i-1, c.j); haswall1[c.i][c.j]=false;}
+    }
+}
+
+let ind = Math.floor(Math.random()*n);
+if (Math.random() < 0.5){
+    if (Math.random() < 0.5){
+        haswall0[ind][0] = false;
+    }else{
+        haswall0[ind][n] = false;
+    }
+}else{
+    if (Math.random() < 0.5){
+        haswall1[0][ind] = false;
+    }else{
+        haswall1[n][ind] = false;
+    }
+}
+
 var nw = 0;
 var wall = [], t = [];
 
@@ -14,10 +73,14 @@ function addWall(type, x, z){
     ++nw;
 }
 
-for (let x=-225; x<=225; x+=9){
-    for (let z=-225; z<=225; z+=9){
-        if (x<225) addWall(0, x, z-4.5);
-        if (z<225) addWall(1, x-4.5, z);
+for (let i=0; i<n; ++i){
+    for (let j=0; j<=n; ++j){
+        if (haswall0[i][j]) addWall(0, 9*i, 9*j-4.5);
+    }
+}
+for (let i=0; i<=n; ++i){
+    for (let j=0; j<n; ++j){
+        if (haswall1[i][j]) addWall(1, 9*i-4.5, 9*j);
     }
 }
 
@@ -31,14 +94,14 @@ scene.add( light );
 scene.add( light2 );
 scene.add( light3 );
 
-var cx = 0, cy = 0, cz = 0;
+var cx = 9*n/2, cy = 0, cz = 9*n/2;
 var alpha=-Math.PI/2, beta=0;
 updateCamera();
 function updateCamera(){
     camera.position.set(cx, cy, cz);
     camera.lookAt(new THREE.Vector3(Math.cos(beta)*Math.cos(alpha) + cx, Math.sin(beta) + cy, Math.cos(beta)*Math.sin(alpha) + cz));
 }
-var vel = 0.5;
+var vel = 0.2;
 function update() {
     let oldx = cx;
     let oldz = cz;
@@ -95,8 +158,8 @@ function mouseup() {
 	console.log("Mouse clicked at", mouseX, mouseY);
 }
 function mouseMove(mx, my){
-    alpha += mx/300;
-    beta -= my/300;
+    alpha += mx/700;
+    beta -= my/700;
     if (beta >= Math.PI/2) beta = Math.PI/2;
     if (beta <= -Math.PI/2) beta = -Math.PI/2;
 }
