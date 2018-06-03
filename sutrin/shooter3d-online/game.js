2bd53381ac55238bcf3417c782a63e069e7305fa
Ivo Dilov <iddilov@gmail.com>
2018-06-03 11:30:56 +0300

    sending locations

--- sutrin/shooter3d-online/game.js
+++ sutrin/shooter3d-online/game.js
@@ -1,15 +1,56 @@
-// Creating variables
 var socket = io();
 
 var canvas = document.getElementsByTagName('canvas')[0];
 var geometry = new THREE.BoxGeometry( 2, 2, 2 );
 var material = new THREE.MeshPhongMaterial({color: 'red'});
 
+class Player{
+    constructor(x, y, z, alpha, beta){
+        this.m = new THREE.Mesh(geometry, material);
+        this.move(x, y, z, alpha, beta);
+        scene.add(this.m);
+    }
+    move(x, y, z, alpha, beta){
+        this.m.position.set(x, y, z);
+        this.m.rotation.y = alpha;
+        //this.m.rotation.y = beta;
+    }
+}
+
 var floorg = new THREE.BoxGeometry(1000, 1, 1000);
 var floorm = new THREE.MeshBasicMaterial({color: 'green'});
 var floor = new THREE.Mesh(floorg, floorm);
 floor.position.set(0, -5, 0)
 scene.add(floor);
+
+var p = [];
+var id = -1;
+var velocity = 0.1;
+var alpha = Math.PI/2, beta = 0;
+var cx=1, cy=0 , cz=1, dy=0;
+
+socket.on('init', function(cid, pl){
+    id = cid;
+    for (let i=0; i<pl.length; ++i){
+        if (i == id) {
+            cx = pl[i].x;
+            cy = pl[i].y;
+            cz = pl[i].z;
+            alpha = pl[i].alpha;
+            beta = pl[i].beta;
+        }else{
+            p[i] = new Player(pl[i].x, pl[i].y, pl[i].z, pl[i].alpha, pl[i].beta);
+        }
+    }
+});
+
+socket.on('newpl', function(pid, pl){
+    p[pid]=new Player(pl.x, pl.y, pl.z, pl.alpha, pl.beta);
+});
+
+socket.on('mv', function(pid, pl){
+    p[pid].move(pl.x, pl.y, pl.z, pl.alpha, pl.beta);
+});
 
 var light = new THREE.PointLight( );
 var light2 = new THREE.PointLight( );
@@ -21,18 +62,18 @@ scene.add( light );
 scene.add( light2 );
 scene.add( light3 );
 
-var velocity = 0.1;
-var alpha = Math.PI/2, beta = 0;
-var cx=1, cy=0 , cz=1, dy=0;
 function updateCamera(){   
     camera.position.set(cx, cy, cz);
     camera.lookAt(new THREE.Vector3(Math.cos(alpha)*Math.cos(beta) + cx, Math.sin(beta) + cy, Math.sin(alpha)*Math.cos(beta) + cz));
 }
 updateCamera();
 
+var oldx, oldy, oldz, oldalpha, oldbeta;
 function update() {
+    if (id == -1) return;
     dy-=0.01;
     oldx = cx;
+    oldy = cy;
     oldz = cz;
     cy+=dy;
     if (cy<0) cy=0;
@@ -49,6 +90,11 @@ function update() {
         cx += Math.cos(alpha+Math.PI/2)*velocity*5;
         cz += Math.sin(alpha+Math.PI/2)*velocity*5;
     }
+    if (oldx != cx || oldy != cy || oldz != cz || oldalpha != alpha || oldbeta != beta){
+        socket.emit('mv', cx, cy, cz, alpha, beta);
+    }
+    oldalpha = alpha;
+    oldbeta = beta;
     updateCamera();
 }
 
--- sutrin/shooter3d-online/server.js
+++ sutrin/shooter3d-online/server.js
@@ -12,9 +12,17 @@ app.get('/three.min.js', function(req, res){
     res.sendFile(__dirname + "/three.min.js");
 })
 
-
+var pl = [];
+var numpl = 0;
 io.on('connection', function(socket){
-    
+    let cid=numpl++;
+    pl.push({x: Math.random()*1000-500, y: 0, z: Math.random()*1000-500, alpha: 0, beta: 0});
+    socket.emit('init', cid, pl);
+    socket.broadcast.emit('newpl', cid, pl[cid]);
+    socket.on('mv', function(x, y, z, alpha, beta){
+        pl[cid] = {x: x, y: y, z: z, alpha: alpha, beta: beta};
+        socket.broadcast.emit('mv', cid, pl[cid]);
+    });
 });
 
 http.listen(3000, function(){
